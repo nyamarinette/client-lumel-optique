90f49da9698b56c9986b915d21a46cddfa772bbc
nyamarinette <nyamarinette2020@gmail.com>
2025-09-27 10:01:12 +0200

    produits modifies

--- src/components/SectionDesProduits.jsx
+++ src/components/SectionDesProduits.jsx
@@ -46,11 +46,11 @@ export default function SectionDesProduits() {
               </div>
 
               <div className="mt-1 p-2">
-                <h2 className="text-slate-700">Lunettes de Vue</h2>
+                <h2 className="text-slate-700">Lunettes de Vue | 45000 Fcfa </h2>
                 <p className="mt-1 text-sm text-slate-400">Noir, Homme</p>
 
                 <div className="mt-3 flex items-end justify-between">
-                  <p className="text-lg font-bold text-blue-500">$850</p>
+                  <p className="text-lg font-bold text-blue-500">new</p>
 
                   <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                     <svg
@@ -98,12 +98,12 @@ export default function SectionDesProduits() {
 
               <div className="mt-1 p-2">
                 <h2 className="text-slate-700">
-                  Lunettes de Soleil avec ou sans Prescription
+                 Lunettes de Vue | 45000 Fcfa
                 </h2>
                 <p className="mt-1 text-sm text-slate-400">Douala, Cameroun</p>
 
                 <div className="mt-3 flex items-end justify-between">
-                  <p className="text-lg font-bold text-blue-500">$850</p>
+                  <p className="text-lg font-bold text-blue-500">new</p>
 
                   <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                     <svg
@@ -159,12 +159,12 @@ export default function SectionDesProduits() {
 
               <div className="mt-1 p-2">
                 <h2 className="text-slate-700">
-                  Lentilles de Contact et Produits d’Entretien
+                  Lunettes de Vue | 45000 Fcfa
                 </h2>
                 <p className="mt-1 text-sm text-slate-400">Douala, Cameroun</p>
 
                 <div className="mt-3 flex items-end justify-between">
-                  <p className="text-lg font-bold text-blue-500">$450</p>
+                  <p className="text-lg font-bold text-blue-500">new</p>
                   <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -219,12 +219,12 @@ export default function SectionDesProduits() {
 
               <div className="mt-1 p-2">
                 <h2 className="text-slate-700">
-                  Produits pour Yeux Secs et Hygiène des Paupières
+                 Lunettes de Vue | 45000 Fcfa
                 </h2>
                 <p className="mt-1 text-sm text-slate-400">Douala, Cameroun</p>
 
                 <div className="mt-3 flex items-end justify-between">
-                  <p className="text-lg font-bold text-blue-500">$450</p>
+                  <p className="text-lg font-bold text-blue-500">new</p>
                   <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -270,65 +270,7 @@ export default function SectionDesProduits() {
           {/*   Produits de Maisons à louer     */}
 
           
-            <a href="#">
-              <div className="relative flex items-end overflow-hidden rounded-xl">
-                <Image
-                  src="https://maria-bucket-1.s3.us-east-1.amazonaws.com/D%C3%A9veloppeuse+Web/Les+plateformes+web+de+ma+client%C3%A8le/Bethel+Eyes+Care_Site+Internet/Les+images+du+site+internet/Produits/Produits_maison/maison_4.jpg"
-                  alt="black framed eyeglasses"
-                  width={500}
-                  height={500}
-                />
-                <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                    />
-                  </svg>
-
-                  <button className="text-sm">Contactez l&apos;agent</button>
-                </div>
-              </div>
-
-              <div className="mt-1 p-2">
-                <h2 className="text-slate-700">Maison à vendre ($734,900)</h2>
-                <p className="mt-1 text-sm italic font-bold text-slate-400">
-                  Virginia Beach, USA
-                </p>
-
-                <div className="mt-3 flex items-end justify-between">
-                  <p className="text-lg font-bold text-blue-500">new</p>
-
-                  <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="h-4 w-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                      />
-                    </svg>
-
-                    <button className="text-sm">Contactez l&apos;agent</button>
-                  </div>
-                </div>
-              </div>
-            </a>
-          
+           
         </div>
       </section>
     </section>
